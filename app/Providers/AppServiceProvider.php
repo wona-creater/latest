07c74980a749ca0d148446470fc85aa894640789
FortuneJames <oriebuluechendu123@gmail.com>
2025-04-12 05:26:27 +0100

    schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\shema\Builder;
+use Illuminate\Database\schema\Builder;
 
 
 class AppServiceProvider extends ServiceProvider
